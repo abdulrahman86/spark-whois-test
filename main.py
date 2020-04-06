2e89d0f053f4f38e1a0689e7dfc805aaf95e12fe
abdulsattar <abdulrahman86@gmail.com>
2020-04-06 17:55:21 -0400

    DomainInfo as spark struct type

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ spark = SparkSession.builder.appName('Basics').master("local[1]").getOrCreate()
 
 def domain_info(domain_name):
     d = DomainInfo(domain_name)
-    return d.get_domain_age_in_days()
+    return [d.get_domain_age_in_days(), d.get_domain_registrar(), d.get_domain_expiration()]
 
 
 a_list = [(1, 'www.microsoft.com'), (2, 'www.google.com'), (3, 'www.facebook1234512345.com')]
 
-domain_info_udf = udf(domain_info, LongType())
+
+domain_info_udf = udf(domain_info, StructType(
+    [
+        StructField("domain_age", LongType(), True),
+        StructField("domain_registrar", StringType(), True),
+        StructField("domain_expiration", StringType(), True)
+    ]))
 
 df = spark.createDataFrame(a_list, ['id', 'domain_name'])
 
-df = df.select("id", "domain_name", domain_info_udf("domain_name").alias("domain_age"))
+df = df.select("id", "domain_name", domain_info_udf("domain_name").alias("domain_info"))
+
+df = df.select("id", "domain_name",
+               df["domain_info.domain_age"], df["domain_info.domain_registrar"], df["domain_info.domain_expiration"])
 
 df.show()
 
--- model/DomainInfo.py
+++ model/DomainInfo.py
@@ -3,11 +3,25 @@ import whois
 
 class DomainInfo:
     def __init__(self, domain):
-        self.w = whois.query(domain)
+        self.whois_info = whois.query(domain)
+
 
     def get_domain_age_in_days(self):
-        if (self.w is not None):
+        if (self.whois_info is not None):
             today = datetime.today()
-            return (today - self.w.creation_date).days
+            return (today - self.whois_info.creation_date).days
         else:
             return -1
+
+    def get_domain_registrar(self):
+        if (self.whois_info is not None):
+            return self.whois_info.registrar
+        else:
+            return None
+
+    def get_domain_expiration(self):
+        if (self.whois_info is not None):
+            return self.whois_info.expiration_date.strftime("%m/%d/%Y, %H:%M:%S")
+        else:
+            return None
+
